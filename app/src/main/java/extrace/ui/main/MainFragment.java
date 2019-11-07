bc1172a5b477497b4e201c4465b2039446c10af5
xzsk2 <sk308269317@outlook.com>
2019-11-07 16:28:05 +0800

    修复乱码 修改主页UI 实现客户管理中的搜索

--- app/src/main/java/extrace/ui/main/MainFragment.java
+++ app/src/main/java/extrace/ui/main/MainFragment.java
@@ -6,22 +6,26 @@ import extrace.ui.domain.PackagePaisongActivity;
 import extrace.ui.domain.PackageTransActivity;
 import extrace.ui.domain.ExpressEditActivity;
 import extrace.ui.misc.CustomerListActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-public class MainFragment  extends Fragment {
-	
+import static android.content.ContentValues.TAG;
+
+public class MainFragment extends Fragment {
+
     /**
      * Returns a new instance of this fragment for the given section
      * number.
      */
     public static MainFragment newInstance() {
-    	MainFragment fragment = new MainFragment();
+        MainFragment fragment = new MainFragment();
         Bundle args = new Bundle();
         //args.putInt(ARG_SECTION_NUMBER, sectionNumber);
         fragment.setArguments(args);
@@ -33,213 +37,146 @@ public class MainFragment  extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);	//加载布局
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);    //加载布局
         //fragment_main中的操作激发
-        rootView.findViewById(R.id.action_ex_receive_icon).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View view) {
-						StartReceiveExpress();
-					}
-				});
         rootView.findViewById(R.id.action_ex_receive).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View view) {
-						StartReceiveExpress();
-					}
-				});												//快件揽收对应的文本框和图标
-        
-        
-        rootView.findViewById(R.id.action_ex_transfer_icon).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View view) {
-						PaiSong();
-					}
-				});
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Log.d(TAG, "onClick: dawidjawiofawfwaf");
+                        StartReceiveExpress();
+                    }
+                });                                                //快件揽收
+
+
         rootView.findViewById(R.id.action_ex_transfer).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View view) {
-						PaiSong();
-					}
-				});													//快件派送对应的文本框和图标
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        PaiSong();
+                    }
+                });                                                    //快件派送对应的文本框和图标
 
 
-       /*
-        * 新增打包,拆包
-        */  
-        rootView.findViewById(R.id.action_pk_pkg_icon).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View view) {
-						 PackExpress();
-					}
-				});
+        /*
+         * 新增打包,拆包
+         */
+
         rootView.findViewById(R.id.action_pk_pkg).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View view) {
-						 PackExpress();
-					}
-				});														//打包
-        
-        rootView.findViewById(R.id.action_pk_exp_icon).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View view) {
-						 UnPackExpress();
-					}
-				});
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        PackExpress();
+                    }
+                });                                                        //打包
+
+
         rootView.findViewById(R.id.action_pk_exp).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View view) {
-						 UnPackExpress();
-					}
-				});														//拆包
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        UnPackExpress();
+                    }
+                });                                                        //拆包
 
 
+        rootView.findViewById(R.id.action_pk_transport).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        TransPackage();
+                    }
+                });
 
 
-
-		rootView.findViewById(R.id.action_pk_transport_icon).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View view) {
-						TransPackage();
-					}
-				});
-		rootView.findViewById(R.id.action_pk_transport_icon).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View view) {
-						TransPackage();
-					}
-				});
-
-
-
-
-
-
-
-		rootView.findViewById(R.id.action_cu_mng_icon).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View view) {
-						StartCustomerList();
-					}
-				});
         rootView.findViewById(R.id.action_cu_mng).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View view) {
-						StartCustomerList();
-					}
-				});
-        																								//客户管理对应的文本框和图标
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        StartCustomerList();
+                    }
+                });
+        //客户管理对应的文本框和图标
 
-        rootView.findViewById(R.id.action_ex_qur_icon).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View view) {
-						StartQueryExpress();
-					}
-				});
+
         rootView.findViewById(R.id.action_ex_qur).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View view) {
-						StartQueryExpress();
-					}
-				});													//快件查询对应的文本框和图标
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        StartQueryExpress();
+                    }
+                });                                                    //快件查询对应的文本框和图标
 
 
-		rootView.findViewById(R.id.action_ex_history_icon).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View view) {
-						esHistory();
-					}
-				});
-		rootView.findViewById(R.id.action_ex_history).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View view) {
-						esHistory();
-					}
-				});												   //运输历史对应的文本框和图标
+        rootView.findViewById(R.id.action_ex_history).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        esHistory();
+                    }
+                });                                                   //运输历史对应的文本框和图标
 
 
-		return rootView;
-    }
-    
-    void StartReceiveExpress()
-    {
-		Intent intent = new Intent();
-		intent.putExtra("Action","New");
-		intent.setClass(this.getActivity(), ExpressEditActivity.class);
-		startActivityForResult(intent, 0);  	
+        return rootView;
     }
 
-    void StartQueryExpress()
-    {
-		Intent intent = new Intent();
-		intent.putExtra("Action","Query");
-		intent.setClass(this.getActivity(), ExpressEditActivity.class);
-		startActivityForResult(intent, 0);  	
+    void StartReceiveExpress() {
+        Intent intent = new Intent();
+        intent.putExtra("Action", "New");
+        intent.setClass(this.getActivity(), ExpressEditActivity.class);
+        startActivityForResult(intent, 0);
     }
 
-    void StartCustomerList()
-    {
-		Intent intent = new Intent();
-		intent.putExtra("Action","None");
-		intent.setClass(this.getActivity(), CustomerListActivity.class);
-		startActivityForResult(intent, 0);
+    void StartQueryExpress() {
+        Intent intent = new Intent();
+        intent.putExtra("Action", "Query");
+        intent.setClass(this.getActivity(), ExpressEditActivity.class);
+        startActivityForResult(intent, 0);
     }
-    
+
+    void StartCustomerList() {
+        Intent intent = new Intent();
+        intent.putExtra("Action", "None");
+        intent.setClass(this.getActivity(), CustomerListActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
     //以下新增
 
-	void PaiSong(){
-		Intent intent = new Intent();
-		intent.setClass(this.getActivity(), PackagePaisongActivity.class);
-		startActivityForResult(intent, 0);
-	}
-    
-    void PackExpress()
-    {
-		Intent intent = new Intent();
-		intent.putExtra("Action","pack");
-		intent.setClass(this.getActivity(), PackageEditActivity.class);
-		startActivityForResult(intent, 0);  	
-    }
-    void UnPackExpress()
-    {
-		Intent intent = new Intent();
-		intent.putExtra("Action","unPack");
-		intent.setClass(this.getActivity(), PackageEditActivity.class);
-		startActivityForResult(intent, 0);  	
+    void PaiSong() {
+        Intent intent = new Intent();
+        intent.setClass(this.getActivity(), PackagePaisongActivity.class);
+        startActivityForResult(intent, 0);
     }
 
-	void TransPackage()
-	{
-		Intent intent = new Intent();
-		//intent.putExtra("Action","unPack");
-		intent.setClass(this.getActivity(), PackageTransActivity.class);
-		startActivityForResult(intent, 0);
-	}
+    void PackExpress() {
+        Intent intent = new Intent();
+        intent.putExtra("Action", "pack");
+        intent.setClass(this.getActivity(), PackageEditActivity.class);
+        startActivityForResult(intent, 0);
+    }
 
-	void esHistory()
-	{
-		Intent intent = new Intent();
-		intent.putExtra("Action","esHistory");
-		intent.setClass(this.getActivity(), PackageDeliverActivity.class);
-		startActivityForResult(intent, 0);
-	}
+    void UnPackExpress() {
+        Intent intent = new Intent();
+        intent.putExtra("Action", "unPack");
+        intent.setClass(this.getActivity(), PackageEditActivity.class);
+        startActivityForResult(intent, 0);
+    }
 
+    void TransPackage() {
+        Intent intent = new Intent();
+        //intent.putExtra("Action","unPack");
+        intent.setClass(this.getActivity(), PackageTransActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+    void esHistory() {
+        Intent intent = new Intent();
+        intent.putExtra("Action", "esHistory");
+        intent.setClass(this.getActivity(), PackageDeliverActivity.class);
+        startActivityForResult(intent, 0);
+    }
 
 
 }
